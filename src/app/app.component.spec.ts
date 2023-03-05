72168b7058d113b719afcf4bb6d26427a1e059f7
Thomas Freund <a00831007@itesm.mx>
2023-03-05 14:04:28 -0600

    changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'mycv'`, () => {
+  it(`should have as title 'Thomas Freund CV'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('mycv');
+    expect(app.title).toEqual('Thomas Freund CV');
   });
 
   it('should render title', () => {
