0b798512aeb6ffb9579abb77be3655a27c1819f3
Thomas Freund <a00831007@itesm.mx>
2023-03-05 21:37:58 -0600

    Correcting build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { WorkExperienceComponent } from './work-experience/work-experience.component';
+ import { WorkExperienceComponent } from './work-experience/work-experience.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
+ 
     WorkExperienceComponent
   ],
   imports: [
